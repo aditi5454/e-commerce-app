36c38d8a76bfa71c9fdbd422a8bb0bde77eb0e27
aditi5454 <aditigarg2028@gmail.com>
2022-11-28 02:44:40 +0530

    minor changes

---
+++ src/pages/home/components/Image-Slider/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./image-slider.css"
+
+const ImageSlider =() =>{
+    return <div className="image-slider-component">this is image slider</div>
+}
+
+export default ImageSlider;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./home.css"
 import Header from "../../common/components/header"
 import Navbar from "../../common/components/navbar"
+import ImageSlider from "./components/Image-Slider/index"
 
 const Home =() =>{
     return <div className="home-page">
     <Header /> 
     <Navbar />
-    <section className=" common-section image-slider">Image Slider</section>
+    <ImageSlider />
     <section className=" common-section offers">popular offers</section>
     <section className= "common-section payment-means">payments</section>
     {/* <Footer />*/}
