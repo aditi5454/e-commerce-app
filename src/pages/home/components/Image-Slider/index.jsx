fb497770b3e3470b1cea4ac4971f024b718c594a
aditi5454 <aditigarg2028@gmail.com>
2022-11-29 08:36:59 +0530

    image-slider

--- src/pages/home/components/Image-Slider/index.jsx
+++ src/pages/home/components/Image-Slider/index.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React,{ useState } from "react";
 import "./image-slider.css"
+import Carousel from 'react-bootstrap/Carousel';
 
-const ImageSlider =() =>{
-    return <div className="image-slider-component">this is image slider</div>
+import Carousel4 from "./../../../../common/assets/images/carousel-4.webp";
+import Carousel5 from "./../../../../common/assets/images/carousel-5.webp";
+import Carousel7 from "./../../../../common/assets/images/carousel-7.webp";
+
+const ImageSlider=()=> {
+    const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect} >
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Carousel7}
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Carousel5}
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Carousel4}
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
 export default ImageSlider;
