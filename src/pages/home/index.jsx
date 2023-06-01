a5748e2816f2c41110cab412a5569b45c824d7a1
aditi garg <108354311+aditi5454@users.noreply.github.com>
2023-06-01 22:03:43 +0530

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./home.css"
 import Header from "../../common/components/header"
 import Navbar from "../../common/components/navbar"
-import ImageSlider from "./components/image-slider/index"
+import ImageSlider from "./components/Image-Slider/index"
 import Features from "./components/features/index"
 import Footer from "../../common/components/footer";
 import PopularOffers from "./components/popular-offers/index";
