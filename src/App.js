f2a6fca8ab341f24850690e643830d52f9ad4d88
aditi5454 <aditigarg2028@gmail.com>
2024-10-05 00:20:36 +0530

    integrated tailwind

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import Login from './pages/login';
-import Register from './pages/register';
-import Home from './pages/home';
-import About from './pages/home/components/about';
-import ProductList from './pages/product-list';
-import ProductDetails from './pages/product-details';
-import CartPage from './pages/cart-page';
-import PaymentPage from './pages/payment';
-import OrderPlaced from './pages/order-placed';
-import OrderFailed from './pages/order-failed';
-import CartAddress from './pages/cart-address';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Home from "./pages/home";
+import About from "./pages/home/components/about";
+import ProductList from "./pages/product-list";
+import ProductDetails from "./pages/product-details";
+import CartPage from "./pages/cart-page";
+import PaymentPage from "./pages/payment";
+import OrderPlaced from "./pages/order-placed";
+import OrderFailed from "./pages/order-failed";
+import CartAddress from "./pages/cart-address";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import { Routes, Route } from "react-router-dom";
-import {useEffect} from 'react';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-
   useEffect(() => {
-    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   }, []);
-  
+
   return (
     <div className="App">
       <Routes>
@@ -35,12 +34,13 @@ function App() {
         <Route path="/order-placed" element={<OrderPlaced />} />
         <Route path="/order-failed" element={<OrderFailed />} />
         <Route path="/cart-address" element={<CartAddress />} />
-        <Route path="/product-list/:productName/:productId" element={<ProductDetails />} />
+        <Route
+          path="/product-list/:productName/:productId"
+          element={<ProductDetails />}
+        />
       </Routes>
     </div>
   );
 }
 
 export default App;
-
-
--- src/common/components/footer/index.jsx
+++ src/common/components/footer/index.jsx
@@ -1,62 +1,112 @@
 import React from "react";
-import "./footer.css"
-import Facebook from "./../../assets/icons/footer-facebook.png"
-import Insta from "./../../assets/icons/footer-insta.png"
-import Pinterest from "./../../assets/icons/footer-pinterest.png"
-import Twitter from "./../../assets/icons/footer-twitter.png"
-import YouTube from "./../../assets/icons/footer-youtube.png"
+import "./footer.css";
+import Facebook from "./../../assets/icons/footer-facebook.png";
+import Insta from "./../../assets/icons/footer-insta.png";
+import Pinterest from "./../../assets/icons/footer-pinterest.png";
+import Twitter from "./../../assets/icons/footer-twitter.png";
+import YouTube from "./../../assets/icons/footer-youtube.png";
 import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
-    const navigate= useNavigate();
-    const goToAboutPage=()=>{
-        navigate("/about");
-    }
-    
-    return <div className="footer-component">
-        <div className="footer-container">
-            <div className="footer-div1">
-                <h5 className="footer-headings">SKY COLLECTION</h5>
-                <div className="footer-info">(123)-456-7890</div>
-                <div className="footer-info">Support@skycollection121.com</div>
-            </div>
-            <div className="footer-div2">
-                <h6 className="footer-headings">SKY COLLECTION</h6>
-                <ul>
-                <a href="/home" className="footer-anchor-li"><li className="footer-li">Home</li></a>
-                <a href="/product-list" className="footer-anchor-li"><li className="footer-li">All</li></a>
-                <a href="/product-list" className="footer-anchor-li"><li className="footer-li">Tops</li></a>
-                <a href="/product-list" className="footer-anchor-li"><li className="footer-li">Bottom</li></a>
-                <a href="/product-list" className="footer-anchor-li"><li className="footer-li">Dresses</li></a>
-                <a href="/product-list" className="footer-anchor-li"><li className="footer-li">Ethnic Wear</li></a>
-                <a href="/product-list" className="footer-anchor-li"><li className="footer-li">Footwear</li></a>
-                <li className="footer-li" onClick={goToAboutPage}>About Us</li>
-                </ul>
-                
-            </div>
-            <div className="footer-div3">
-                <h6 className="footer-headings">customer service</h6>
-                <ul>
-                <a href="/cart-page" className="footer-anchor-li"><li className="footer-li" >my cart</li></a>
-                <a href="#" className="footer-anchor-li"><li className="footer-li">shipping</li></a>
-                <li className="footer-li">return policy</li>
-                <li className="footer-li">FAQ</li>
-                <li className="footer-li">contact</li>
-                </ul>
-            </div>
-            <div className="footer-div4">
-                <h6 className="footer-headings">social</h6>
-                <div className="footer-logos">
-                    <a href="https://www.facebook.com/" ><img src={Facebook} alt="facebook-logo" className="footer-logo"></img>
-                    </a>
-                    <a href="https://www.instagram.com/" ><img src={Insta} alt="instagram-logo" className="lofooter-logogos"></img></a>
-                    <a href="https://twitter.com/" ><img src={Twitter} alt="twitter-logo" className="footer-logo"></img></a>
-                    <a href="https://in.pinterest.com/" ><img src={Pinterest} alt="pinterest-logo" className="footer-logo"></img></a>
-                    <a href="https://www.youtube.com/" ><img src={YouTube} alt="youtube-logo" className="footer-logo"></img></a>
-                </div>
-            </div>
+  const navigate = useNavigate();
+  const goToAboutPage = () => {
+    navigate("/about");
+  };
+
+  return (
+    <div className="footer-component">
+      <div className="footer-container">
+        <div className="w-[450px] pl-20">
+          <h5 className="text-left pl-8 font-semibold">SKY COLLECTION</h5>
+          <div className="text-left pl-8">(123)-456-7890</div>
+          <div className="text-left pl-8">Support@skycollection121.com</div>
         </div>
+        <div className="footer-div2">
+          <h6 className="text-left pl-8 font-semibold">SKY COLLECTION</h6>
+          <ul>
+            <a href="/home" className="footer-anchor-li">
+              <li className="footer-li">Home</li>
+            </a>
+            <a href="/product-list" className="footer-anchor-li">
+              <li className="footer-li">All</li>
+            </a>
+            <a href="/product-list" className="footer-anchor-li">
+              <li className="footer-li">Tops</li>
+            </a>
+            <a href="/product-list" className="footer-anchor-li">
+              <li className="footer-li">Bottom</li>
+            </a>
+            <a href="/product-list" className="footer-anchor-li">
+              <li className="footer-li">Dresses</li>
+            </a>
+            <a href="/product-list" className="footer-anchor-li">
+              <li className="footer-li">Ethnic Wear</li>
+            </a>
+            <a href="/product-list" className="footer-anchor-li">
+              <li className="footer-li">Footwear</li>
+            </a>
+            <li className="footer-li" onClick={goToAboutPage}>
+              About Us
+            </li>
+          </ul>
+        </div>
+        <div className="footer-div3">
+          <h6 className="text-left pl-8 font-semibold">customer service</h6>
+          <ul>
+            <a href="/cart-page" className="footer-anchor-li">
+              <li className="footer-li">my cart</li>
+            </a>
+            <a href="#" className="footer-anchor-li">
+              <li className="footer-li">shipping</li>
+            </a>
+            <li className="footer-li">return policy</li>
+            <li className="footer-li">FAQ</li>
+            <li className="footer-li">contact</li>
+          </ul>
+        </div>
+        <div className="footer-div4">
+          <h6 className="text-left pl-8 font-semibold">social</h6>
+          <div className="footer-logos">
+            <a href="https://www.facebook.com/">
+              <img
+                src={Facebook}
+                alt="facebook-logo"
+                className="footer-logo"
+              ></img>
+            </a>
+            <a href="https://www.instagram.com/">
+              <img
+                src={Insta}
+                alt="instagram-logo"
+                className="lofooter-logogos"
+              ></img>
+            </a>
+            <a href="https://twitter.com/">
+              <img
+                src={Twitter}
+                alt="twitter-logo"
+                className="footer-logo"
+              ></img>
+            </a>
+            <a href="https://in.pinterest.com/">
+              <img
+                src={Pinterest}
+                alt="pinterest-logo"
+                className="footer-logo"
+              ></img>
+            </a>
+            <a href="https://www.youtube.com/">
+              <img
+                src={YouTube}
+                alt="youtube-logo"
+                className="footer-logo"
+              ></img>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-}
+  );
+};
 
 export default Footer;
